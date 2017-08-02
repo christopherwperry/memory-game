8ff1952531abec063bb8fcdd46af1905562aae36
christopherwperry <christopherwperry@gmail.com>
2017-08-01 20:44:45 -0400

    added js to fill game card divs and have them shuffle each time

---
+++ script.js
@@ -0,0 +1,75 @@
+// Establish array that contains all the images for the "backs" of cards.
+
+let imageArray = [];
+
+imageArray[0] = new Image();
+imageArray[0].src = "images/aliens.jpg";
+imageArray[1] = new Image();
+imageArray[1].src = "images/aliens.jpg";
+imageArray[2] = new Image();
+imageArray[2].src = "images/doge.jpg";
+imageArray[3] = new Image();
+imageArray[3].src = "images/doge.jpg";
+imageArray[4] = new Image ();
+imageArray[4].src = "images/arthur.jpg";
+imageArray[5] = new Image ();
+imageArray[5].src = "images/arthur.jpg";
+imageArray[6] = new Image();
+imageArray[6].src = "images/fellowkids.jpg";
+imageArray[7] = new Image();
+imageArray[7].src = "images/fellowkids.jpg";
+imageArray[8] = new Image();
+imageArray[8].src = "images/fry.jpg";
+imageArray[9] = new Image();
+imageArray[9].src = "images/fry.jpg";
+imageArray[10] = new Image ();
+imageArray[10].src = "images/kermit.jpg";
+imageArray[11] = new Image ();
+imageArray[11].src = "images/kermit.jpg";
+imageArray[12] = new Image();
+imageArray[12].src = "images/conceit.jpg";
+imageArray[13] = new Image();
+imageArray[13].src = "images/conceit.jpg";
+imageArray[14] = new Image();
+imageArray[14].src = "images/pepe.jpg";
+imageArray[15] = new Image();
+imageArray[15].src = "images/pepe.jpg";
+imageArray[16] = new Image ();
+imageArray[16].src = "images/trap.jpg";
+imageArray[17] = new Image ();
+imageArray[17].src = "images/trap.jpg";
+imageArray[18] = new Image ();
+imageArray[18].src = "images/mjcrying.jpg";
+imageArray[19] = new Image ();
+imageArray[19].src = "images/mjcrying.jpg";
+
+function shuffle(imageArray) {
+  let i = 0;
+  let j = 0;
+  let temp = null;
+
+  for (i = imageArray.length-1; i > 0; i -= 1) {
+    j = Math.floor(Math.random()*(i + 1));
+    temp = imageArray[i];
+    imageArray[i] = imageArray[j];
+    imageArray[j] = temp;
+  }
+  return imageArray;
+}
+
+let shuffledArray = shuffle(imageArray);
+let createDiv = document.createElement("div");
+let gameCont = document.getElementById("game-container");
+
+function createCards(){
+  for(let i = 0; i < shuffledArray.length; i++){
+    let cardCase = document.createElement("div");
+    let cardPic = document.createElement("div");
+    cardCase.setAttribute("class", "game-cards");
+    cardPic.setAttribute("class", "card-pics");
+    cardPic.appendChild(shuffledArray[i]);
+    cardCase.appendChild(cardPic);
+    gameCont.appendChild(cardCase);
+  }
+}
+gameCont.appendChild(createCards())
